66656a482efe2d7d0bdca2de963e1adb26e1690b
Daniele Bonini (皮夕): WebDev,  DevOps and project care by lots of Simplicity bits <my25mb@aol.com>
2021-10-18 14:32:45 +0200

    Add files via upload

--- js/common.js
+++ js/common.js
@@ -65,9 +65,9 @@ function mymain() {
 
 } 
 
-$(document).ready(function() {           
+window.addEventListener("load", function() {
   mymain();
-}
+}};
 
 window.addEventListener("resize", function() {
   mymain();
