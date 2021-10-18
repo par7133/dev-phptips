759d684ff63f4f38531aa299c210334e09748fef
Daniele Bonini (皮夕): WebDev,  DevOps and project care by lots of Simplicity bits <my25mb@aol.com>
2021-10-18 13:04:26 +0200

    Add files via upload

--- js/common.js
+++ js/common.js
@@ -47,7 +47,7 @@ function setContent() {
   });  
   $(".cat").each(function(){
     if (bodyRect.width < 660) {
-      $(this).css("width", "97%");
+      $(this).css("width", "96%");
       $(this).css("padding", "15px");
       $(this).css("background-color", "#4c5d8d");
       $(this).css("margin-bottom", "5px");
